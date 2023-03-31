e88ddfa8e154cda975e5ec469576b6e7e766af02
Aman07a <aman16.aa17@gmail.com>
2023-03-31 14:19:31 +0200

    Dynamic Array Creation

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { User } from "./User";
 
 const user = new User({ name: "Aman", age: 22 });
 
-user.set({ age: 23 });
+user.on("change", () => {});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+console.log(user);
