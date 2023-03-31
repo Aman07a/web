d42ad35639bbc665a12de1552405cc8e3726b798
Aman07a <aman16.aa17@gmail.com>
2023-03-31 14:22:45 +0200

    Triggering Event Callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import { User } from "./User";
 
 const user = new User({ name: "Aman", age: 22 });
 
-user.on("change", () => {});
+user.on("change", () => {
+  console.log("Change #1");
+});
 
-console.log(user);
+user.on("change", () => {
+  console.log("Change #2");
+});
+
+user.on("change", () => {
+  console.log("Save was triggered");
+});
+
+user.trigger("change");
