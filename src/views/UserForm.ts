ee7b72515940d2150fb92f25dbff7e33c04060af
Aman07a <aman16.aa17@gmail.com>
2023-04-01 20:52:02 +0200

    Binding Events on Class Name

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -5,25 +5,23 @@ export class UserForm {
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
-      "mouseenter:h1": this.onHeaderHover,
+      "click:.set-age": this.onSetAgeClick,
     };
   }
 
-  onHeaderHover(): void {
-    console.log("H1 was hovered over");
-  }
-
-  onButtonClick(): void {
-    console.log("Hi there");
+  onSetAgeClick(): void {
+    console.log("button was clicked");
   }
 
   template(): string {
     return `
         <div>
             <h1>User Form</h1>
+            <div>User name: ${this.model.get("name")}</div>
+            <div>User age: ${this.model.get("age")}</div>
             <input />
             <button>Click Me</button>
+            <button class="set-age">Set Random Age</button>
         </div>
     `;
   }
@@ -34,7 +32,7 @@ export class UserForm {
     for (let eventKey in eventsMap) {
       const [eventName, selector] = eventKey.split(":");
 
-      fragment.querySelectorAll(selector).forEach((element) => {
+      fragment.querySelectorAll(".set-age").forEach((element) => {
         element.addEventListener(eventName, eventsMap[eventKey]);
       });
     }
